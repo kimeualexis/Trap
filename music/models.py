1d0b68a0a8d05f353c43abedae6ac3609691e061
kimeualexis <kimeualexis@gmail.com>
2019-04-03 20:03:30 +0300

    base template

--- music/models.py
+++ music/models.py
@@ -8,10 +8,16 @@ class Album(models.Model):
     album_genre = models.CharField(max_length=10)
     album_cover = models.FileField()
 
+    def __str__(self):
+        return self.album_name
+
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     song_name = models.CharField(max_length=25)
     song_audio = models.FileField()
+
+    def __str__(self):
+        return self.song_name
 
 
